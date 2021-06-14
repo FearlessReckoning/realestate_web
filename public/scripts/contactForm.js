fa737aa5306c3316190b47a01bb41681337e1f72
Rarencibia <40923510+Rarencibia@users.noreply.github.com>
2021-06-14 00:31:16 -0400

    add new pictures of Jose and minor UI changes

--- public/scripts/contactForm.js
+++ public/scripts/contactForm.js
@@ -1,20 +1,37 @@
-document.getElementById("formName").addEventListener("click", (e)=> {
-    e.currentTarget.style.display ="none";
-    formNumber.style.display = "flex"
-});
-document.getElementById("formNumber").addEventListener("click", (e) => {
-    e.currentTarget.style.display ="none";
-    formEmail.style.display = "flex";
-});
-document.getElementById("formEmail").addEventListener("click", (e) => {
-    e.currentTarget.style.display = "none";
-    formReason.style.display = "flex";
-});
-document.getElementById("formReason").addEventListener("click", (e) => {
-    e.currentTarget.style.display="none";
-    formHow.style.display = "flex";
-});
-document.getElementById("formHow").addEventListener("click", (e) => {
-    e.currentTarget.style.display = "none";
-});
+const contactButton = document.getElementsByClassName("contactButton");
+
+
+addGlobalEventListener("click", "contactButton", e => {
+    e.target.style.display = "none";
+})
+
+function addGlobalEventListener(type, selector, callback){
+    document.addEventListener(type, e => {
+        if(e.target.matches(selector)) callback(e)
+    });
+
+}
+
+
+
+// document.getElementById("formName").addEventListener("click", (e)=> {
+//     e.currentTarget.style.display ="none";
+//     formNumber.style.display = "flex"
+// });
+// document.getElementById("formNumber").addEventListener("click", (e) => {
+//     e.currentTarget.style.display ="none";
+//     formEmail.style.display = "flex";
+// });
+// document.getElementById("formEmail").addEventListener("click", (e) => {
+//     e.currentTarget.style.display = "none";
+//     formReason.style.display = "flex";
+// });
+// document.getElementById("formReason").addEventListener("click", (e) => {
+//     e.currentTarget.style.display="none";
+//     formHow.style.display = "flex";
+// });
+// document.getElementById("formHow").addEventListener("click", (e) => {
+//     e.currentTarget.style.display = "none";
+// });
+
 
