e203f2681c3893af3e035322b7693db8ebf77faa
Myles <myles.leadbeater@gmail.com>
2021-06-28 16:12:25 +0930

    m

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var listingsRouter = require('./routes/listings');
 var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/listings', listingsRouter);
 app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/listings.js
+++ routes/listings.js
@@ -3,11 +3,19 @@ var router = express.Router();
 
 
 router.get('/', function(req, res, next) {
-  res.render('listings', { title: 'Express' });
+    // if we are logged in
+        // res.render('private/listings', { title: 'Express' });
+    res.render('public/listings', { title: 'Express' });
 });
 
 router.get('/edit', function(req, res, next) {
-  res.render('listings_edit', { title: 'Express' });
+    // if we are logged in
+    res.render('listings_edit', { title: 'Express' });
+});
+
+router.get('/new', function(req, res, next) {
+    // if we are logged in
+    res.render('listings_new', { title: 'Express' });
 });
 
 module.exports = router;
